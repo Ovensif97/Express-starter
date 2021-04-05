b36b131fbe21f729f6425dca3f10d989fd2cbfce
Ovensif <imamtaufikt.ti.poliwangi@gmail.com>
2021-04-05 13:42:01 +0700

    Progress on querying, check if email already registered or not | Auth Register

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Register router below
 var example = require('./routes/example');
+let Auth = require('./routes/AuthRouter');
 
 // If you wanna put middleware just input here!
 // const middleware = require('./middleware/example');
@@ -21,7 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Declare middleware and sub url below
 app.use('/', example);
+app.use('/Auth/', Auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
